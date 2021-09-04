7bbb15c7780435aa68ae62867bd6657752fb6e17
Pascal Knoth <io.pascal.knoth@gmail.com>
2021-09-04 20:46:45 +0200

    do not raise an error when `client_secret` is missing

--- lib/boruta/oauth/authorization/client.ex
+++ lib/boruta/oauth/authorization/client.ex
@@ -15,117 +15,136 @@ defmodule Boruta.Oauth.Authorization.Client do
       {:ok, %Boruta.Oauth.Client{...}}
   """
   @spec authorize(
-    [id: String.t(), secret: String.t()] |
-    [id: String.t(), secret: String.t(), grant_type: String.t()] |
-    [id: String.t(), redirect_uri: String.t(), grant_type: String.t()] |
-    [id: String.t(), redirect_uri: String.t(), grant_type: String.t(), code_verifier: String.t()]
-  ) ::
-    {:ok, Client.t()}
-    | {:error,
-      %Error{
-        :error => :invalid_client,
-        :error_description => String.t(),
-        :format => nil,
-        :redirect_uri => nil,
-        :status => :unauthorized
-      }}
-  def authorize(id: id, secret: secret) do
+          [id: String.t(), secret: String.t()]
+          | [id: String.t(), secret: String.t(), grant_type: String.t()]
+          | [id: String.t(), redirect_uri: String.t(), grant_type: String.t()]
+          | [
+              id: String.t(),
+              redirect_uri: String.t(),
+              grant_type: String.t(),
+              code_verifier: String.t()
+            ]
+        ) ::
+          {:ok, Client.t()}
+          | {:error,
+             %Error{
+               :error => :invalid_client,
+               :error_description => String.t(),
+               :format => nil,
+               :redirect_uri => nil,
+               :status => :unauthorized
+             }}
+  def authorize(id: id, secret: secret) when not is_nil(id) and not is_nil(secret) do
     case ClientsAdapter.get_by(id: id, secret: secret) do
       %Client{} = client ->
         {:ok, client}
+
       nil ->
         {:error,
-          %Error{
-            status: :unauthorized,
-            error: :invalid_client,
-            error_description: "Invalid client_id or client_secret."
-          }
-        }
+         %Error{
+           status: :unauthorized,
+           error: :invalid_client,
+           error_description: "Invalid client_id or client_secret."
+         }}
     end
   end
-  def authorize(id: id, secret: secret, grant_type: grant_type) do
-    with %Client
-      {supported_grant_types: supported_grant_types
-    } = client <- ClientsAdapter.get_by(id: id, secret: secret),
+
+  def authorize(id: id, secret: secret, grant_type: grant_type)
+      when not is_nil(id) and not is_nil(secret) do
+    with %Client{supported_grant_types: supported_grant_types} = client <-
+           ClientsAdapter.get_by(id: id, secret: secret),
          true <- Enum.member?(supported_grant_types, grant_type) do
-        {:ok, client}
+      {:ok, client}
     else
       nil ->
         {:error,
-          %Error{
-            status: :unauthorized,
-            error: :invalid_client,
-            error_description: "Invalid client_id or client_secret."
-          }
-        }
+         %Error{
+           status: :unauthorized,
+           error: :invalid_client,
+           error_description: "Invalid client_id or client_secret."
+         }}
+
       false ->
         {:error,
-          %Error{
-            status: :bad_request,
-            error: :unsupported_grant_type,
-            error_description: "Client do not support given grant type."
-          }
-        }
+         %Error{
+           status: :bad_request,
+           error: :unsupported_grant_type,
+           error_description: "Client do not support given grant type."
+         }}
     end
   end
+
   def authorize(id: id, redirect_uri: redirect_uri, grant_type: grant_type) do
     with %Client{
-      supported_grant_types: supported_grant_types
-      } = client <- ClientsAdapter.get_by(id: id, redirect_uri: redirect_uri),
+           supported_grant_types: supported_grant_types
+         } = client <- ClientsAdapter.get_by(id: id, redirect_uri: redirect_uri),
          true <- Enum.member?(supported_grant_types, grant_type) do
-        {:ok, client}
+      {:ok, client}
     else
       nil ->
         {:error,
-          %Error{
-            status: :unauthorized,
-            error: :invalid_client,
-            error_description: "Invalid client_id or redirect_uri."
-          }
-        }
+         %Error{
+           status: :unauthorized,
+           error: :invalid_client,
+           error_description: "Invalid client_id or redirect_uri."
+         }}
+
       false ->
         {:error,
-          %Error{
-            status: :bad_request,
-            error: :unsupported_grant_type,
-            error_description: "Client do not support given grant type."
-          }
-        }
+         %Error{
+           status: :bad_request,
+           error: :unsupported_grant_type,
+           error_description: "Client do not support given grant type."
+         }}
     end
   end
-  def authorize(id: id, redirect_uri: redirect_uri, grant_type: grant_type, code_verifier: code_verifier) do
+
+  def authorize(
+        id: id,
+        redirect_uri: redirect_uri,
+        grant_type: grant_type,
+        code_verifier: code_verifier
+      ) do
     with %Client{
-      supported_grant_types: supported_grant_types
-      } = client <- ClientsAdapter.get_by(id: id, redirect_uri: redirect_uri),
+           supported_grant_types: supported_grant_types
+         } = client <- ClientsAdapter.get_by(id: id, redirect_uri: redirect_uri),
          :ok <- validate_pkce(client, code_verifier),
          true <- Enum.member?(supported_grant_types, grant_type) do
-        {:ok, client}
+      {:ok, client}
     else
       nil ->
         {:error,
-          %Error{
-            status: :unauthorized,
-            error: :invalid_client,
-            error_description: "Invalid client_id or redirect_uri."
-          }
-        }
+         %Error{
+           status: :unauthorized,
+           error: :invalid_client,
+           error_description: "Invalid client_id or redirect_uri."
+         }}
+
       false ->
         {:error,
-          %Error{
-            status: :bad_request,
-            error: :unsupported_grant_type,
-            error_description: "Client do not support given grant type."
-          }
-        }
+         %Error{
+           status: :bad_request,
+           error: :unsupported_grant_type,
+           error_description: "Client do not support given grant type."
+         }}
+
       {:error, :invalid_pkce_request} ->
         {:error,
-          %Error{
-            status: :bad_request,
-            error: :invalid_request,
-            error_description: "PKCE request invalid."
-          }
-        }
+         %Error{
+           status: :bad_request,
+           error: :invalid_request,
+           error_description: "PKCE request invalid."
+         }}
     end
+  end
+
+  def authorize(_params) do
+    {:error,
+     %Error{
+       status: :unauthorized,
+       error: :invalid_client,
+       error_description: "Invalid client_id or client_secret."
+     }}
   end
 
   defp validate_pkce(%Client{pkce: false}, _code_verifier), do: :ok
--- test/boruta/oauth/integration/refresh_token_test.exs
+++ test/boruta/oauth/integration/refresh_token_test.exs
@@ -15,106 +15,199 @@ defmodule Boruta.OauthTest.RefreshTokenTest do
 
   describe "refresh_token" do
     setup do
-      stub(ResourceOwners, :get_by, fn (_params) -> {:error, "No resource owner."} end)
+      stub(ResourceOwners, :get_by, fn _params -> {:error, "No resource owner."} end)
       client = insert(:client)
       client_without_grant_type = insert(:client, supported_grant_types: [])
-      expired_access_token = insert(
-        :token,
-        type: "access_token",
-        refresh_token: Boruta.TokenGenerator.generate(),
-        client: client,
-        redirect_uri: List.first(client.redirect_uris),
-        expires_at: :os.system_time(:seconds) - 10
-      )
+
+      expired_access_token =
+        insert(
+          :token,
+          type: "access_token",
+          refresh_token: Boruta.TokenGenerator.generate(),
+          client: client,
+          redirect_uri: List.first(client.redirect_uris),
+          expires_at: :os.system_time(:seconds) - 10
+        )
+
       {:ok, revoked_at} = (:os.system_time(:seconds) - 10) |> DateTime.from_unix()
-      revoked_access_token = insert(
-        :token,
-        type: "access_token",
-        refresh_token: Boruta.TokenGenerator.generate(),
-        client: client,
-        redirect_uri: List.first(client.redirect_uris),
-        revoked_at: revoked_at
-      )
-      access_token = insert(
-        :token,
-        type: "access_token",
-        refresh_token: Boruta.TokenGenerator.generate(),
-        client: client,
-        redirect_uri: List.first(client.redirect_uris),
-        expires_at: :os.system_time(:seconds) + 10,
-        scope: "scope"
-      )
-      other_client_access_token = insert(
-        :token,
-        type: "access_token",
-        refresh_token: Boruta.TokenGenerator.generate(),
-        client: insert(:client),
-        redirect_uri: List.first(client.redirect_uris),
-        expires_at: :os.system_time(:seconds) + 10,
-        scope: "scope"
-      )
+
+      revoked_access_token =
+        insert(
+          :token,
+          type: "access_token",
+          refresh_token: Boruta.TokenGenerator.generate(),
+          client: client,
+          redirect_uri: List.first(client.redirect_uris),
+          revoked_at: revoked_at
+        )
+
+      access_token =
+        insert(
+          :token,
+          type: "access_token",
+          refresh_token: Boruta.TokenGenerator.generate(),
+          client: client,
+          redirect_uri: List.first(client.redirect_uris),
+          expires_at: :os.system_time(:seconds) + 10,
+          scope: "scope"
+        )
+
+      other_client_access_token =
+        insert(
+          :token,
+          type: "access_token",
+          refresh_token: Boruta.TokenGenerator.generate(),
+          client: insert(:client),
+          redirect_uri: List.first(client.redirect_uris),
+          expires_at: :os.system_time(:seconds) + 10,
+          scope: "scope"
+        )
+
       {:ok,
-        client: client,
-        client_without_grant_type: client_without_grant_type,
-        expired_access_token: expired_access_token,
-        revoked_access_token: revoked_access_token,
-        access_token: access_token,
-        other_client_access_token: other_client_access_token
-      }
+       client: client,
+       client_without_grant_type: client_without_grant_type,
+       expired_access_token: expired_access_token,
+       revoked_access_token: revoked_access_token,
+       access_token: access_token,
+       other_client_access_token: other_client_access_token}
     end
 
     test "returns an error if `grant_type` is 'refresh_token' and schema is invalid" do
-      assert Oauth.token(%Plug.Conn{body_params: %{"grant_type" => "refresh_token"}}, ApplicationMock) == {:token_error, %Error{
-        error: :invalid_request,
-        error_description: "Request body validation failed. Required property refresh_token is missing at #.",
-        status: :bad_request
-      }}
+      assert Oauth.token(
+               %Plug.Conn{body_params: %{"grant_type" => "refresh_token"}},
+               ApplicationMock
+             ) ==
+               {:token_error,
+                %Error{
+                  error: :invalid_request,
+                  error_description:
+                    "Request body validation failed. Required property refresh_token is missing at #.",
+                  status: :bad_request
+                }}
     end
 
     test "returns an error if client is invalid" do
-      %{req_headers: [{"authorization", authorization_header}]} = using_basic_auth("6a2f41a3-c54c-fce8-32d2-0324e1c32e22", "test")
-      assert Oauth.token(%Plug.Conn{
-        body_params: %{"grant_type" => "refresh_token", "refresh_token" => "refresh_token"},
-        req_headers: [{"authorization", authorization_header}]
-      }, ApplicationMock) == {:token_error, %Error{
-        error: :invalid_client,
-        error_description: "Invalid client_id or client_secret.",
-        status: :unauthorized
-      }}
+      %{req_headers: [{"authorization", authorization_header}]} =
+        using_basic_auth("6a2f41a3-c54c-fce8-32d2-0324e1c32e22", "test")
+
+      assert Oauth.token(
+               %Plug.Conn{
+                 body_params: %{
+                   "grant_type" => "refresh_token",
+                   "refresh_token" => "refresh_token"
+                 },
+                 req_headers: [{"authorization", authorization_header}]
+               },
+               ApplicationMock
+             ) ==
+               {:token_error,
+                %Error{
+                  error: :invalid_client,
+                  error_description: "Invalid client_id or client_secret.",
+                  status: :unauthorized
+                }}
+    end
+
+    test "returns an error if `client_id` is missing" do
+      assert Oauth.token(
+               %Plug.Conn{
+                 body_params: %{
+                   "grant_type" => "refresh_token",
+                   "refresh_token" => "refresh_token"
+                 }
+               },
+               ApplicationMock
+             ) ==
+               {:token_error,
+                %Error{
+                  error: :invalid_client,
+                  error_description: "Invalid client_id or client_secret.",
+                  status: :unauthorized
+                }}
+    end
+
+    test "returns an error if `client_secret` is missing" do
+      assert Oauth.token(
+               %Plug.Conn{
+                 body_params: %{
+                   "client_id" => "6a2f41a3-c54c-fce8-32d2-0324e1c32e22",
+                   "grant_type" => "refresh_token",
+                   "refresh_token" => "refresh_token"
+                 }
+               },
+               ApplicationMock
+             ) ==
+               {:token_error,
+                %Error{
+                  error: :invalid_client,
+                  error_description: "Invalid client_id or client_secret.",
+                  status: :unauthorized
+                }}
     end
 
     test "returns an error if client is absent" do
-      assert Oauth.token(%Plug.Conn{
-        body_params: %{"grant_type" => "refresh_token", "refresh_token" => "refresh_token"},
-      }, ApplicationMock) == {:token_error, %Error{
-        error: :invalid_client,
-        error_description: "Invalid client_id or client_secret.",
-        status: :unauthorized
-      }}
+      assert Oauth.token(
+               %Plug.Conn{
+                 body_params: %{
+                   "grant_type" => "refresh_token",
+                   "refresh_token" => "refresh_token"
+                 }
+               },
+               ApplicationMock
+             ) ==
+               {:token_error,
+                %Error{
+                  error: :invalid_client,
+                  error_description: "Invalid client_id or client_secret.",
+                  status: :unauthorized
+                }}
     end
 
     test "returns an error if refresh_token is invalid", %{client: client} do
-      %{req_headers: [{"authorization", authorization_header}]} = using_basic_auth(client.id, client.secret)
-      assert Oauth.token(%Plug.Conn{
-        body_params: %{"grant_type" => "refresh_token", "refresh_token" => "bad_refresh_token"},
-        req_headers: [{"authorization", authorization_header}]
-      }, ApplicationMock) == {:token_error, %Error{
-        error: :invalid_refresh_token,
-        error_description: "Provided refresh token is incorrect.",
-        status: :bad_request
-      }}
+      %{req_headers: [{"authorization", authorization_header}]} =
+        using_basic_auth(client.id, client.secret)
+
+      assert Oauth.token(
+               %Plug.Conn{
+                 body_params: %{
+                   "grant_type" => "refresh_token",
+                   "refresh_token" => "bad_refresh_token"
+                 },
+                 req_headers: [{"authorization", authorization_header}]
+               },
+               ApplicationMock
+             ) ==
+               {:token_error,
+                %Error{
+                  error: :invalid_refresh_token,
+                  error_description: "Provided refresh token is incorrect.",
+                  status: :bad_request
+                }}
     end
 
-    test "returns an error if access_token associated is expired", %{client: client, expired_access_token: token} do
-      %{req_headers: [{"authorization", authorization_header}]} = using_basic_auth(client.id, client.secret)
-      assert Oauth.token(%Plug.Conn{
-        body_params: %{"grant_type" => "refresh_token", "refresh_token" => token.refresh_token},
-        req_headers: [{"authorization", authorization_header}]
-      }, ApplicationMock) == {:token_error, %Error{
-        error: :invalid_refresh_token,
-        error_description: "Token expired.",
-        status: :bad_request
-      }}
+    test "returns an error if access_token associated is expired", %{
+      client: client,
+      expired_access_token: token
+    } do
+      %{req_headers: [{"authorization", authorization_header}]} =
+        using_basic_auth(client.id, client.secret)
+
+      assert Oauth.token(
+               %Plug.Conn{
+                 body_params: %{
+                   "grant_type" => "refresh_token",
+                   "refresh_token" => token.refresh_token
+                 },
+                 req_headers: [{"authorization", authorization_header}]
+               },
+               ApplicationMock
+             ) ==
+               {:token_error,
+                %Error{
+                  error: :invalid_refresh_token,
+                  error_description: "Token expired.",
+                  status: :bad_request
+                }}
     end
 
     test "returns an error if access_token associated belongs to an other client", %{
@@ -167,80 +260,126 @@ defmodule Boruta.OauthTest.RefreshTokenTest do
                 }}
     end
 
-    test "returns an error if scope is unknown or unauthorized", %{client: client, access_token: token} do
+    test "returns an error if scope is unknown or unauthorized", %{
+      client: client,
+      access_token: token
+    } do
       ResourceOwners
-      |> stub(:authorized_scopes, fn(_resource_owner) -> [] end)
-      %{req_headers: [{"authorization", authorization_header}]} = using_basic_auth(client.id, client.secret)
-      assert Oauth.token(%Plug.Conn{
-        body_params: %{"grant_type" => "refresh_token", "refresh_token" => token.refresh_token, "scope" => "bad_scope"},
-        req_headers: [{"authorization", authorization_header}]
-      }, ApplicationMock) == {:token_error, %Error{
-        error: :invalid_scope,
-        error_description: "Given scopes are unknown or unauthorized.",
-        status: :bad_request
-      }}
+      |> stub(:authorized_scopes, fn _resource_owner -> [] end)
+
+      %{req_headers: [{"authorization", authorization_header}]} =
+        using_basic_auth(client.id, client.secret)
+
+      assert Oauth.token(
+               %Plug.Conn{
+                 body_params: %{
+                   "grant_type" => "refresh_token",
+                   "refresh_token" => token.refresh_token,
+                   "scope" => "bad_scope"
+                 },
+                 req_headers: [{"authorization", authorization_header}]
+               },
+               ApplicationMock
+             ) ==
+               {:token_error,
+                %Error{
+                  error: :invalid_scope,
+                  error_description: "Given scopes are unknown or unauthorized.",
+                  status: :bad_request
+                }}
     end
 
-    test "returns an error if grant type is not allowed by client", %{client_without_grant_type: client, access_token: token} do
-      %{req_headers: [{"authorization", authorization_header}]} = using_basic_auth(client.id, client.secret)
-      assert Oauth.token(%Plug.Conn{
-        body_params: %{"grant_type" => "refresh_token", "refresh_token" => token.refresh_token, "scope" => "bad_scope"},
-        req_headers: [{"authorization", authorization_header}]
-      }, ApplicationMock) == {:token_error, %Error{
-        error: :unsupported_grant_type,
-        error_description: "Client do not support given grant type.",
-        status: :bad_request
-      }}
+    test "returns an error if grant type is not allowed by client", %{
+      client_without_grant_type: client,
+      access_token: token
+    } do
+      %{req_headers: [{"authorization", authorization_header}]} =
+        using_basic_auth(client.id, client.secret)
+
+      assert Oauth.token(
+               %Plug.Conn{
+                 body_params: %{
+                   "grant_type" => "refresh_token",
+                   "refresh_token" => token.refresh_token,
+                   "scope" => "bad_scope"
+                 },
+                 req_headers: [{"authorization", authorization_header}]
+               },
+               ApplicationMock
+             ) ==
+               {:token_error,
+                %Error{
+                  error: :unsupported_grant_type,
+                  error_description: "Client do not support given grant type.",
+                  status: :bad_request
+                }}
     end
 
     test "returns token", %{client: client, access_token: token} do
       ResourceOwners
-      |> stub(:authorized_scopes, fn(_resource_owner) -> [] end)
-      %{req_headers: [{"authorization", authorization_header}]} = using_basic_auth(client.id, client.secret)
+      |> stub(:authorized_scopes, fn _resource_owner -> [] end)
+
+      %{req_headers: [{"authorization", authorization_header}]} =
+        using_basic_auth(client.id, client.secret)
+
       case Oauth.token(
-        %Plug.Conn{
-          body_params: %{"grant_type" => "refresh_token", "refresh_token" => token.refresh_token, "scope" => "scope"},
-          req_headers: [{"authorization", authorization_header}]
-        },
-        ApplicationMock
-      ) do
+             %Plug.Conn{
+               body_params: %{
+                 "grant_type" => "refresh_token",
+                 "refresh_token" => token.refresh_token,
+                 "scope" => "scope"
+               },
+               req_headers: [{"authorization", authorization_header}]
+             },
+             ApplicationMock
+           ) do
         {:token_success,
-          %TokenResponse{
-            token_type: token_type,
-            access_token: access_token,
-            expires_in: expires_in,
-            refresh_token: refresh_token,
-          }
-        } ->
+         %TokenResponse{
+           token_type: token_type,
+           access_token: access_token,
+           expires_in: expires_in,
+           refresh_token: refresh_token
+         }} ->
           assert token_type == "bearer"
           assert access_token
           assert expires_in
           assert refresh_token
+
         _ ->
           assert false
       end
     end
 
-    test "returns token with associated access_token scope as default", %{client: client, access_token: token} do
+    test "returns token with associated access_token scope as default", %{
+      client: client,
+      access_token: token
+    } do
       ResourceOwners
-      |> stub(:authorized_scopes, fn(_resource_owner) -> [] end)
-      %{req_headers: [{"authorization", authorization_header}]} = using_basic_auth(client.id, client.secret)
+      |> stub(:authorized_scopes, fn _resource_owner -> [] end)
+
+      %{req_headers: [{"authorization", authorization_header}]} =
+        using_basic_auth(client.id, client.secret)
+
       case Oauth.token(
-        %Plug.Conn{
-          body_params: %{"grant_type" => "refresh_token", "refresh_token" => token.refresh_token},
-          req_headers: [{"authorization", authorization_header}]
-        },
-        ApplicationMock
-      ) do
+             %Plug.Conn{
+               body_params: %{
+                 "grant_type" => "refresh_token",
+                 "refresh_token" => token.refresh_token
+               },
+               req_headers: [{"authorization", authorization_header}]
+             },
+             ApplicationMock
+           ) do
         {:token_success,
-          %TokenResponse{
-            access_token: access_token
-          }
-        } ->
+         %TokenResponse{
+           access_token: access_token
+         }} ->
           expected_scope = token.scope
+
           assert %Ecto.Token{
-            scope: ^expected_scope
-          } = Repo.get_by(Ecto.Token, value: access_token)
+                   scope: ^expected_scope
+                 } = Repo.get_by(Ecto.Token, value: access_token)
+
         _ ->
           assert false
       end
