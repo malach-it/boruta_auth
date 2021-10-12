1252e236f72488c98d46604b907953ff28e1c0ee
Pascal Knoth <io.pascal.knoth@gmail.com>
2021-10-12 13:27:53 +0200

    bump to 2.0.0-rc.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Boruta.MixProject do
     [
       name: "Boruta core",
       app: :boruta,
-      version: "1.2.0",
+      version: "2.0.0-rc.0",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
