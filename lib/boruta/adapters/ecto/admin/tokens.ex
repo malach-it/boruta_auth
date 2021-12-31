968ddaa21f141eeb979f9b5ca90b9db72f0b5f2f
Pascal Knoth <io.pascal.knoth@gmail.com>
2021-12-31 13:48:36 +0100

    `Admin.delete_inactive_tokens/0,1` implementation

--- lib/boruta/adapters/ecto/admin/tokens.ex
+++ lib/boruta/adapters/ecto/admin/tokens.ex
@@ -20,10 +20,32 @@ defmodule Boruta.Ecto.Admin.Tokens do
   @spec list_active_tokens() :: active_tokens :: list(Token.t())
   @spec list_active_tokens(queryable :: Ecto.Queryable.t()) :: active_tokens :: list(Token.t())
   def list_active_tokens(queryable \\ Token) do
+    queryable
+    |> active_tokens_query()
+    |> repo().all()
+  end
+
+  @spec delete_inactive_tokens() ::
+          {number_of_deleted_tokens :: integer(), deleted_tokens :: list(Token.t())}
+  @spec delete_inactive_tokens(until :: DateTime.t()) ::
+          {number_of_deleted_tokens :: integer(), deleted_tokens :: list(Token.t())}
+  def delete_inactive_tokens(until \\ DateTime.utc_now()) do
+    until = DateTime.to_unix(until)
+    from(t in Token, as: :parent)
+    |> where([t], t.expires_at < ^until)
+    |> where(
+      [parent: t],
+      not exists(active_tokens_query() |> where([t], parent_as(:parent).id == t.id))
+    )
+    |> select([t], t)
+    |> repo().delete_all()
+  end
+
+  defp active_tokens_query(queryable \\ Token) do
     now = :os.system_time(:seconds)
+
     queryable
     |> where([t], t.expires_at >= ^now)
     |> where([t], is_nil(t.revoked_at))
-    |> repo().all()
   end
 end
