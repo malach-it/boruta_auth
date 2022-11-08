591e51cc8a7ec54ef98a219c0b59b001f6c228ce
Pascal Knoth <io.pascal.knoth@gmail.com>
2022-11-08 11:38:57 +0100

    openid client jwks management on dynamic registration

--- test/boruta/openid/integration/dynamic_registration_test.exs
+++ test/boruta/openid/integration/dynamic_registration_test.exs
@@ -18,14 +18,27 @@ defmodule Boruta.OpenidTest.DynamicRegistrationTest do
     end
 
     test "registers a client" do
+      jwk = %{
+        "kty" => "RSA",
+        "e" => "AQAB",
+        "use" => "sig",
+        "alg" => "RS256",
+        "n" =>
+          "iN2CZVIKWB--I5yxqQtwLWncQR_N7u7Ge0bE3zqj4tqKVSHgBEE3xobV-nOKisAJzCy1QhJb7Cy9MQYxBZ09HbAXvZVHVFRtrTcFk87ZcB_7H8T_Nh_uydJEjiW--ryP1klNefa9V4t3WCwmNgX1ipP0ZHhNenemOT9BASQyF-_5Gm7KsDxJ8DkZH_OQhl5xdqXwZOh5Y7Cc25ZB1sr9xRse4vah9uiS5YgwTFbGRzS-yIDKuSB8BY1cBT0uwBLICamgI7gV0oZkQ29_ptXPZC1tw3X41eNaPU-G2ocF2vKZwBdGO8weTMfQngjPZ_xKv_y9_Y7P5aF-L3F05eKVjQ"
+      }
+
       redirect_uris = ["http://redirect.uri"]
 
       registration_params = %{
-        redirect_uris: redirect_uris
+        redirect_uris: redirect_uris,
+        jwk: jwk
       }
 
-      assert {:client_registered, %Oauth.Client{redirect_uris: ^redirect_uris}} =
+      assert {:client_registered,
+              %Oauth.Client{redirect_uris: ^redirect_uris, private_key: private_key}} =
                Openid.register_client(:context, registration_params, ApplicationMock)
+
+      assert JOSE.JWK.from_pem(private_key).kty == JOSE.JWK.from_map(jwk).kty
     end
   end
 end
