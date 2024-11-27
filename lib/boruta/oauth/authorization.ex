e2d7c3de4d3b8433a43d8782cc0f5f668ec0b241
Pascal Knoth <pascal@malach.it>
2024-11-27 09:51:05 +0100

    refactor direct post handler + vc+sd-jwt support

--- lib/boruta/oauth/authorization.ex
+++ lib/boruta/oauth/authorization.ex
@@ -975,6 +975,7 @@ defimpl Boruta.Oauth.Authorization, for: Boruta.Oauth.PresentationRequest do
              scope,
              resource_owner.presentation_configuration
            ),
+         # TODO preform a relying_party_redirect_uri verification
          {:ok, client} <-
            (case client_id do
               "did:" <> _key ->
@@ -1054,6 +1055,7 @@ defimpl Boruta.Oauth.Authorization, for: Boruta.Oauth.PresentationRequest do
             response_mode: response_mode
           }} <-
            preauthorize(request) do
+      # TODO create a presentation specific code
       with {:ok, code} <-
              CodesAdapter.create(%{
                resource_owner: %ResourceOwner{sub: sub},
