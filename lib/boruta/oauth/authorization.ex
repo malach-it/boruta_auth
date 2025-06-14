7a67c45eb3188403b7b6f6de5fcb2a174df2e87e
Pascal Knoth <pascal@malach.it>
2025-06-14 16:40:09 +0200

    validate code in presentation chains

--- lib/boruta/oauth/authorization.ex
+++ lib/boruta/oauth/authorization.ex
@@ -1007,6 +1007,14 @@ defimpl Boruta.Oauth.Authorization, for: Boruta.Oauth.PresentationRequest do
                   grant_type: List.first(response_types)
                 )
             end),
+         {:ok, _code} <-
+           (case code do
+              nil ->
+                {:ok, nil}
+
+              code ->
+                Authorization.Code.authorize(%{value: code})
+            end),
          :ok <- Authorization.Nonce.authorize(request),
          :ok <- VerifiableCredentials.validate_authorization_details(authorization_details),
          :ok <- VerifiablePresentations.check_client_metadata(client_metadata),
