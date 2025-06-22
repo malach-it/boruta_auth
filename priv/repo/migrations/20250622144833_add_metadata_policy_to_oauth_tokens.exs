052fbd88bb701d35feae0f740999d3c4e42701b8
Pascal Knoth <pascal@malach.it>
2025-06-22 18:13:31 +0200

    Revert "WIP code bound metadata policy"
    This reverts commit 724af0b03d29d1b30af09ebd4f97f40e8e234daa.

--- priv/repo/migrations/20250622144833_add_metadata_policy_to_oauth_tokens.exs
+++
@@ -1,9 +0,0 @@
-defmodule Boruta.Repo.Migrations.AddMetadataPolicyToOauthTokens do
-  use Ecto.Migration
-
-  def change do
-    alter table(:oauth_tokens) do
-      add :metadata_policy, :jsonb, default: "{}"
-    end
-  end
-end
