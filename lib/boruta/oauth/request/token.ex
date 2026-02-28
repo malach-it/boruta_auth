fb073a0baf91aa318787591132b6cdf4af2e672e
Pascal Knoth <pascal@malach.it>
2026-02-28 17:59:06 +0100

    decrypt oauth token requests / responses

--- lib/boruta/oauth/request/token.ex
+++ lib/boruta/oauth/request/token.ex
@@ -3,6 +3,8 @@ defmodule Boruta.Oauth.Request.Token do
 
   import Boruta.Oauth.Request.Base
 
+  alias Boruta.ClientsAdapter
+  alias Boruta.Oauth
   alias Boruta.Oauth.AuthorizationCodeRequest
   alias Boruta.Oauth.ClientCredentialsRequest
   alias Boruta.Oauth.Error
@@ -24,7 +26,8 @@ defmodule Boruta.Oauth.Request.Token do
                | ClientCredentialsRequest.t()
                | PasswordRequest.t()}
   def request(%{body_params: body_params} = request) do
-    with {:ok, unsigned_params} <- fetch_unsigned_request(request),
+    with {:ok, body_params} <- decrypt_request(body_params),
+         {:ok, unsigned_params} <- fetch_unsigned_request(request),
          {:ok, client_authentication_params} <- fetch_client_authentication(request),
          {:ok, dpop} <- fetch_dpop(request),
          {:ok, params} <-
@@ -46,6 +49,18 @@ defmodule Boruta.Oauth.Request.Token do
          }}
     end
   end
+
+  def decrypt_request(%{"client_id" => client_id, "encrypted_request" => request} = params) when is_binary(request) do
+    with %Oauth.Client{} = client <- ClientsAdapter.get_client(client_id),
+         {:ok, request_params} <- Oauth.Client.Crypto.decrypt(request, client) do
+      {:ok, Map.merge(params, request_params)}
+    else
+      _ ->
+        {:ok, params}
+    end
+  end
+
+  def decrypt_request(params), do: {:ok, params}
 
   defp fetch_dpop(%{req_headers: req_headers}) do
     with {"dpop", dpop} <- List.keyfind(req_headers, "dpop", 0),
